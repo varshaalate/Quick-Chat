74d20110ac8153940dbb898cae7d282d488c29d2
Varsha Alate <varshaalate86@gmail.com>
2025-08-14 11:30:19 +0530

    Initial local commit

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+import Sidebar from '../components/Sidebar'
+import ChatContainer from '../components/ChatContainer'
+import RightSidebar from '../components/RightSidebar'
+
+const HomePage = () => {
+
+  const [selectedUser ,setSelectedUser] = useState(false)
+
+  return (
+    <div className='border w-full h-screen sm:px[15%] sm:py[5%]'>
+     
+    </div>
+  )
+}
+
+export default HomePage
---
+++ src/pages/ProfileFile.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProfileFile = () => {
+  return (
+    <div>
+      <h1>profile Page</h1>
+    </div>
+  )
+}
+
+export default ProfileFile
