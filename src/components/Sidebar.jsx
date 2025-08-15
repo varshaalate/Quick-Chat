4f979b87f118fb4f960895c1d1b0347e86507ac1
Varsha Alate <varshaalate86@gmail.com>
2025-08-15 11:09:50 +0530

    Added some part of sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
-
-const Sidebar = () => {
+import assets from '../assets/assets'
+const Sidebar = ({ selectedUser, setSelectedUser }) => {
   return (
-    <div>
-      
+    <div className='pb-5'>
+      <div className='flex justift-between items-centre'>
+        <img src={assets.logo} alt="Logo" className='max-w-40' />
+        <div className='relative py-2 group'>
+          <img src={assets.menu_icon} alt="Menu" className='max-h-5 cursor-pointer' />
+          <div>
+            <p className='curson-pointer text-sm'>Edit Profile</p>
+            <hr className='my-2 border-t border-gray-500' />
+            <p className='curson-pointer text-sm'>LogOut</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
