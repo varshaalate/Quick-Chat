c174997c0d3174c625b13bd9376a497d8f4c5b96
Varsha Alate <varshaalate86@gmail.com>
2025-08-18 10:35:58 +0530

    Added Sidebar and updated homepage

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
 import assets from '../assets/assets'
+import { useNavigate } from 'react-router-dom'
+
 const Sidebar = ({ selectedUser, setSelectedUser }) => {
+  const navigate = useNavigate();
+
   return (
-    <div className='pb-5'>
-      <div className='flex justift-between items-centre'>
-        <img src={assets.logo} alt="Logo" className='max-w-40' />
-        <div className='relative py-2 group'>
-          <img src={assets.menu_icon} alt="Menu" className='max-h-5 cursor-pointer' />
-          <div>
-            <p className='curson-pointer text-sm'>Edit Profile</p>
-            <hr className='my-2 border-t border-gray-500' />
-            <p className='curson-pointer text-sm'>LogOut</p>
+    <div className={`bg-[#8185B2]/10 h-full p-5 rounded-r-xl overflow-y-scroll text-white ${selectedUser ? "max-md:hidden" : ""}`}>
+      <div className="pb-5">
+        <div className="flex justify-between items-center">
+          <img src={assets.logo} alt="Logo" className="w-40" />
+          <div className="relative py-2 group">
+            <img src={assets.menu_icon} alt="Menu" className="max-h-5 cursor-pointer" />
+            <div className="absolute top-full right-0 mt-2 z-20 w-32 p-5 rounded-md bg-[#282142] border border-gray-600 text-gray-100 hidden group-hover:block">
+              <p onClick={() => navigate('/profile')} className="cursor-pointer text-sm">Edit Profile</p>
+              <hr className="my-2 border-t border-gray-500" />
+              <p className="cursor-pointer text-sm">LogOut</p>
+            </div>
           </div>
         </div>
       </div>
