55ff88d2f87d9b3cced00f82d8077ce05cf8a96b
Varsha Alate <varshaalate86@gmail.com>
2025-08-19 10:22:19 +0530

    Updated the sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,6 +8,8 @@ const Sidebar = ({ selectedUser, setSelectedUser }) => {
   return (
     <div className={`bg-[#8185B2]/10 h-full p-5 rounded-r-xl overflow-y-scroll text-white ${selectedUser ? "max-md:hidden" : ""}`}>
       <div className="pb-5">
+ {/* Icon And that above 3 dot icon */}
+
         <div className="flex justify-between items-center">
           <img src={assets.logo} alt="Logo" className="w-40" />
           <div className="relative py-2 group">
@@ -19,6 +21,17 @@ const Sidebar = ({ selectedUser, setSelectedUser }) => {
             </div>
           </div>
         </div>
+
+
+               {/* Search Bar */}
+        <div className='bg-[#282142] rounded-full flex items-centre gap-2 py-4 px-4 mt-5'>
+          <img src={assets.search_icon} alt="search" className='w-4 h-4 mt-1'/>
+          <input type="text" className='bg-transparent border-none outline-none text-white text-s placeholder-[#c8c8c8] flex-1' placeholder='Search User....'/>
+        </div>
+
+
+
+
       </div>
     </div>
   )
