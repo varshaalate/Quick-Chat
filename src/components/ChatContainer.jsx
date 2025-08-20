a78e508f16715ec099623c64195619ff916afa89
Varsha Alate <varshaalate86@gmail.com>
2025-08-20 22:26:05 +0530

    Completed css of sidebar now adding css for middle chat part

--- src/components/ChatContainer.jsx
+++ src/components/ChatContainer.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import assets from '../assets/assets'
 
 const ChatContainer = () => {
   return (
     <div>
-      <h1>Chat Container</h1>
+      <div className='flex items-center gap-3 py-3 mx-4 border-b border-stone-500'>
+        <img src={assets.profile_martin} alt="" className='w-8 rounded-full'/>
+        <p className='flex-1 text-lg text-white flex items-center gap-2'>
+          Varsha Alate
+          <span className='w-2 h2 rounded-full bg-green-500'></span>
+        </p>
+
+      </div>
     </div>
   )
 }
