d51ead40decd5dd29c1d0c3eb25d63f3077cdd2d
Varsha Alate <varshaalate86@gmail.com>
2025-08-22 23:23:29 +0530

    Added Chat container css

--- src/components/ChatContainer.jsx
+++ src/components/ChatContainer.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import assets from '../assets/assets'
+import assets , {messagesDummyData} from '../assets/assets'
 
 const ChatContainer = ({selectedUser , setSelectedUser}) => {
   return selectedUser?(
     <div className='h-full overflow-scroll relative backdrop-blur-lg'>
+      {/* header */}
       <div className='flex items-center gap-3 py-3 mx-4 border-b border-stone-500'>
         <img src={assets.profile_martin} alt="" className='w-8 rounded-full'/>
         <p className='flex-1 text-lg text-white flex items-center gap-2'>
@@ -12,6 +13,26 @@ const ChatContainer = ({selectedUser , setSelectedUser}) => {
         </p>
       <img src={assets.arrow_icon} alt="" className='md:hidden max-w-7' />
       <img src={assets.help_icon} alt="" className='max-md:hidden max-w-5'/>
+      </div>
+
+      {/* chat  */}
+      <div className='flex flex-col h-[calc(100%-120px)] overflow-x-scroll p-3 pd-6'>
+        {messagesDummyData.map((msg ,index)=>(
+          <div key={index} className={`flex items-end gap-2 justify-end ${msg.senderId !== '680f5116f10f3cd28382ed02' && 'flex-row-reverse'}`}>
+            {msg.image ? (
+              <img src={msg.image} alt="" className='max-w-[230px] border border-gray-700 rounded-lg overflow-hidden mb-8'/>
+            ):(
+              <p className={`p-2 max-w-[200px] md:text-sm font-light rounded-lg mb-8 break-all bg-violet-500/30 text-white ${msg.senderId === '680f5116f10f3cd28382ed02' ? 'rounded-br-none' :'rounded-bl-none'}`}>{msg.text}</p>
+            )}
+
+            <div className='text-center text-xs'>
+              <img src={msg.senderId === '680f5116f10f3cd28382ed02' ? assets.avatar_icon : assets.profile_martin} alt="" className='w-7 rounded-full' />
+            <p className='text-gray-500'>{msg.createdAt}</p>
+            </div>
+
+          </div>
+        ))}
+
       </div>
     </div>
   ):(
