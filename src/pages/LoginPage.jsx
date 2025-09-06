59c3c2fdf7d05e1e1201678fa3c11a2cce548456
Varsha Alate <varshaalate86@gmail.com>
2025-09-06 10:21:20 +0530

    Added CSS for login

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+import assets from '../assets/assets'
 
 const LoginPage = () => {
+
+  const [currState , setCurrState] = useState("Sign up")
+
+
   return (
-    <div>
-      <h1>Login Page</h1>
+    <div className='min-h-screen bg-cover bg-center flex items-center justify-center gap-8 sm:justify-evenly max-sm:flex-col backdrop-blur-2xl'>
+      {/* left Side */}
+      <img src={assets.logo_big} alt="" className='w-[min(30vw,250px)]'/>
+
+      {/* right side */}
+      <form className='border-2 bg-white/8 text-white border-gray-500 p-6 flex flex-col gap-6 rounded-lg shadow-lg'>
+
+      <h2 className='font-medium text-2xl flex justify-between items-center'>{currState}</h2>
+
+      </form>
     </div>
   )
 }
