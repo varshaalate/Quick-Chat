511c9e17eae38953a9db701cb22e54462714addb
Varsha Alate <varshaalate86@gmail.com>
2025-09-15 10:06:17 +0530

    Started to work on backend

--- server/server.js
+++ server/server.js
@@ -3,4 +3,17 @@ import "dotenv/config";
 import cors from "cors";
 import http from "http";
 
-//create express app and http setver
+//create express app and http server
+
+const app = express();
+const server = http.createServer(app);
+
+//Middleware setup
+app.use(express.json({limit:"4mb"}))
+app.use(cors());
+
+app.use("/api/status",(req,res)=>res.send("Server is live"));
+
+const PORT = process.env.PORT || 5000;
+ server.listen(PORT , ()=>console.log("Server is running on Port:" +PORT));
+
