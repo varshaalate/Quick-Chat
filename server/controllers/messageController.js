fa9c62c3f76626bf1ef51e86a8a4df53f1b4f276
Varsha Alate <varshaalate86@gmail.com>
2025-09-27 10:10:59 +0530

    Api's for meassages

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -1,1 +1,38 @@
-import {Message} from '../models/Message.js'
+import { Message } from "../models/Message.js";
+import { User } from "../models/User.js";
+
+//get all users expect logged in user
+export const geyUsersForSideBar = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    //$ne = not equal to
+    //select("-password") = lt will remove password form data
+    const filteredUsers = await User.find({ _id: { $ne: userId } }).select(
+      "-password"
+    );
+
+    //count no. messages not seen
+    const unseenMessages = {};
+    const promises = filteredUsers.map(async (user) => {
+      const messages = await Message.find({
+        sendweId: user._id,
+        receiverId: userId,
+        seen: false,
+      });
+
+      if (messages.length > 0) {
+        unseenMessages[user._id] = messages.length;
+      }
+    });
+
+    await Promise.all(promises);
+    res.json({ success: true, users: filteredUsers, unseenMessages });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+}
+
+
+
+//Get All messeges for  selected  user
