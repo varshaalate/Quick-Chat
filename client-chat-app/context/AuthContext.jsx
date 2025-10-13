367bc12baa27fd2d091eef1c9cfc3dd82983c774
Varsha Alate <varshaalate86@gmail.com>
2025-10-13 23:12:47 +0530

    Added AuthContext

--- client-chat-app/context/AuthContext.jsx
+++ client-chat-app/context/AuthContext.jsx
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import axios from 'axios'
 
 const backendUrl = import.meta.env.VITE_BACKEND_URL;
@@ -6,9 +6,17 @@ axios.defaults.baseURL = backendUrl;
 
 export const AuthContext = createContext();
 
-export const AuthProvider = ({children}) =>{
+export const AuthProvider = ({ children }) => {
+
+    const [token, setToken] = useState(localStorage.getItem("token"));
+    const [authUser, setAuthUser] = useState(null);
+    const [onlineUser, setOnlineUser] = useState([]);
+    const [socket, setSocket] = useState(null);
+
     const value = {
-        
+
+        axios , authUser,onlineUser,socket
+
     }
     return (
         <AuthContext.Provider>
