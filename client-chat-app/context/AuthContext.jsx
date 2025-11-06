08dd0e6119908ef32750d1bc39eec589fc53bc30
Varsha Alate <varshaalate86@gmail.com>
2025-11-06 23:22:47 +0530

    Working on Authentication

--- client-chat-app/context/AuthContext.jsx
+++ client-chat-app/context/AuthContext.jsx
@@ -1,6 +1,7 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import axios from 'axios';
 import toast from "react-hot-toast"
+import {io} from "socket.io-client";
 
 
 const backendUrl = import.meta.env.VITE_BACKEND_URL;
@@ -27,8 +28,31 @@ export const AuthProvider = ({ children }) => {
         }
         catch(error){
                 console.log(error)
+                toast.error(error.message)
         }
     }
+
+    //Connect socket function to handle socket connection and online users updated
+    const connectSocket = (userData) => {
+        if(!userData || socket?.connected) return;
+        const newSocket = io(backendUrl , {
+            query :{userId : userData._id}
+        });
+        newSocket.connect();
+        setSocket(newSocket);
+        newSocket.on("getOnlineUsers" , (userIds) => {
+            setOnlineUser(userIds);
+        })
+    }
+
+
+    //
+    useEffect(() =>{
+        if(token){
+            axios.defaults.headers.common['token'] = token;
+        }
+        checkAuth();
+    })
 
     const value = {
         axios , authUser,onlineUser,socket
